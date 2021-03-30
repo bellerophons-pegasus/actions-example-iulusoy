1427f983f9c3bb88af109bf0c0529ac07ee5a5dd
bellerophons-pegasus <bellerophons_pegasus@yahoo.de>
2021-03-30 10:13:21 +0200

    linter

--- src/package/filter14.py
+++ src/package/filter14.py
@@ -1,10 +1,9 @@
 # filter14.py
 # Module filter for team14-software
 
-import pandas as pd
-
 def filterdata(data, threshold=0, keeplist=[]):
-    """Filters data based on variance. If variance is below a given threshold, column is discarded
+    """Filters data based on variance. If variance is below a given threshold, \
+    column is discarded
 
     :param data: The data to be filtered.
     :type data: pandas dataframe (dict)
@@ -12,9 +11,11 @@ def filterdata(data, threshold=0, keeplist=[]):
     :type threshold: float , optional
     :param keeplist: List with names of columns to keep.
     :type keeplist: list, optional
-    :returns: A filtered dataframe, where columns with variance below the threshold were removed
+    :returns: A filtered dataframe, where columns with variance below the \
+    threshold were removed
     :rtype: dataframe
     """
+
 
     for i in data.keys():
         if i in keeplist:
